e7b40f2bc4b54c46a4bb05e236fab7ab6eca03f3
TRex22 <contact@jasonchalom.com>
2016-05-12 17:22:54 +0200

    cmd

---
+++ src/NetAndCuda/Image_Processor/convert csv results.cmd
@@ -0,0 +1,3 @@
+@echo off
+rem ImageProcessor batch c:\dataTest\node\output c:\dataTest\node\bw convert bw csv 100 100
+ImageProcessor batch C:\dataTest\node\result c:\dataTest\node\final_conv convert rgb png 100 100
